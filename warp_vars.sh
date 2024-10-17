eb644bcc7192f18c56616bea07ffce59469c66f3
passeway <164740303+passeway@users.noreply.github.com>
2024-10-17 09:46:37 +0800

    Update warp_vars.sh

--- warp_vars.sh
+++ warp_vars.sh
@@ -3,7 +3,7 @@
 # 运行 warp-reg 并提取输出
 output=$(curl -sLo warp-reg https://github.com/badafans/warp-reg/releases/download/v1.0/main-linux-arm64 && chmod +x warp-reg && ./warp-reg && rm warp-reg)
 
-# 使用 jq 提取需要的字段
+# 使用 grep 提取需要的字段
 device_id=$(echo "$output" | grep -oP '(?<=device_id: ).*')
 token=$(echo "$output" | grep -oP '(?<=token: ).*')
 account_id=$(echo "$output" | grep -oP '(?<=account_id: ).*')
@@ -12,6 +12,7 @@ private_key=$(echo "$output" | grep -oP '(?<=private_key: ).*')
 public_key=$(echo "$output" | grep -oP '(?<=public_key: ).*')
 v4=$(echo "$output" | grep -oP '(?<=v4: ).*')
 v6=$(echo "$output" | grep -oP '(?<=v6: ).*')
+reserved=$(echo "$output" | grep -oP '(?<=reserved: \[ ).*(?= \])')
 
 # 输出提取的变量
 echo "Device ID: $device_id"
@@ -20,4 +21,6 @@ echo "Account ID: $account_id"
 echo "License: $license"
 echo "Private Key: $private_key"
 echo "Public Key: $public_key"
+echo "IPv4: $v4"
 echo "IPv6: $v6"
+echo "Reserved: $reserved"
