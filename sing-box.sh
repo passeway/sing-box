cf7d5a3122f65e1b454444294e1769410d7a93e2
passeway <164740303+passeway@users.noreply.github.com>
2024-10-24 16:19:25 +0800

    Update sing-box.sh

--- sing-box.sh
+++ sing-box.sh
@@ -99,18 +99,6 @@ install_sing_box() {
     "timestamp": true,
     "output": "${LOG_FILE}"
   },
-  "dns": {
-    "servers": [
-      {
-        "address": "1.1.1.1",
-        "strategy": "prefer_ipv4"
-      },
-      {
-        "address": "8.8.8.8",
-        "strategy": "prefer_ipv4"
-      }
-    ]
-  },
   "inbounds": [
     {
       "type": "hysteria2",
