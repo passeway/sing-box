4e50374b1df9273869714cf0e0df267dd42d00e4
passeway <164740303+passeway@users.noreply.github.com>
2024-10-26 15:33:09 +0800

    Update sing-box.sh

--- sing-box.sh
+++ sing-box.sh
@@ -149,7 +149,7 @@ install_sing_box() {
         "reality": {
           "enabled": true,
           "handshake": {
-            "server": "1.1.1.1",
+            "server": "www.tesla.com",
             "server_port": 443
           },
           "private_key": "${private_key}",
